5226eac6899cef490f429dd696d64362f1a183a8
Rance SHIH <aika810015jp@gmail.com>
2021-03-01 08:53:45 +0900

    add count

--- guess-num.py
+++ guess-num.py
@@ -1,13 +1,17 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1
 	num = int(input("数字を考えてください："))
 	if num == r:
 		print("当たり！！")
+		print("今回は", count, "回目です！")
 		break
 	elif num > r:
 		print("正解より多いですね！")
 	elif num < r:
 		print("正解より少ないですね！")
+	print("今回は", count, "回目です！")
 
 	
